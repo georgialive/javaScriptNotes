a593529df454bae2e2db74ddd07b6df0b6986d3b
Georgia <georgia.m.vrana@gmail.com>
2023-02-18 15:43:03 -0500

    adding another basic for loop

--- app.js
+++ app.js
@@ -399,24 +399,29 @@ for (let i = 1; i < 100; i++) {
 }
 
 
-// JavaScript Crash Course: Time: 1:05:29
 
 for (let i = 1; i < 20; i++) {
 
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log("Frontend Simplified");
+        console.log(`${i} --> Frontend Simplified`);
     }
 
     else if (i % 3 === 0) {
-        console.log("Frontend");
+        console.log(`${i} --> Frontend`);
     }
 
     else if (i % 5 === 0) {
-        console.log("Simplified");
+        console.log(`${i} --> Simplified`);
     }
 
     else {
-        console.log(i);
+        console.log(`${i} --> ${i}`);
     }
 
+}
+
+const string = "Frontend Simplified"
+
+for (let i = 0; i < string.length; i++) {
+    console.log(string[i]);
 }
